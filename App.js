4436f6209230d368eeda3509fdeb3cd6241081fb
NicolasWan <wanningbang@dxy.cn>
2018-11-28 18:44:08 +0800

    chore: add decorator support

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import codePush from 'react-native-code-push';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,8 +17,8 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+@codePush()
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
